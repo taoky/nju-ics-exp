dd326382e17787e929874a5b192c6fb8a6f38ff7
tracer-ics2018 <tracer@example.com>
2019-01-19 02:22:09 +0000

    >  compile 233333333 ics Linux 585869e0bafe 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  02:22:09 up  7:02,  4 users,  load average: 0.21, 0.11, 0.06 7473bcc8283093586649565840dcaae4ca46811b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,8 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
 static struct {
   char *name;
   char *description;
@@ -46,6 +48,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Instruction level single step", cmd_si },
 
   /* TODO: Add more commands */
 
@@ -74,6 +77,12 @@ static int cmd_help(char *args) {
     printf("Unknown command '%s'\n", arg);
   }
   return 0;
+}
+
+static int cmd_si(char *args) {
+    // char *arg = strtok(NULL, " ");
+    
+    return 0;
 }
 
 void ui_mainloop(int is_batch_mode) {
