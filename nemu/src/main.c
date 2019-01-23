8af2c618b5a8756cc057b253156c52ffd2606853
tracer-ics2018 <tracer@example.com>
2019-01-23 04:23:59 +0000

    >  compile 233333333 ics Linux 585869e0bafe 4.9.125-linuxkit x86_64 unknown unknown GNU/Linux  04:23:59 up 1 day,  4:30,  4 users,  load average: 0.01, 0.04, 0.01 26d3d6e1332489233336f2936762b850c24841a6

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-  /* PA1 Phase 2 Test */
+  /* PA1 Phase 2 Test
   char x;
   printf("Run PA1 Phase2 test? (y/...)");
   x = getchar();
@@ -27,7 +27,8 @@ int main(int argc, char *argv[]) {
               Log("%s=%d success", buf, res);
           }
       }
-  }
+  } */
+
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
