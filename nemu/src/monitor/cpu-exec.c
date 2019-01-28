5e5ed5d66e5fdc8fe0ad457d65f10525b0b3b6ae
tracer-ics2018 <tracer@example.com>
2019-01-28 09:26:17 +0000

    >  compile 233333333 ics Linux 585869e0bafe 4.9.125-linuxkit x86_64 unknown unknown GNU/Linux  09:26:17 up 2 days,  1:15,  7 users,  load average: 0.03, 0.07, 0.04 a53e894fb324e575745000428aa51d72f4f40239

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -69,6 +69,8 @@ void cpu_exec(uint64_t n) {
       if (nemu_state == NEMU_END) {
         printflog("\33[1;31mnemu: HIT %s TRAP\33[0m at eip = 0x%08x\n\n",
             (cpu.eax == 0 ? "GOOD" : "BAD"), cpu.eip - 1);
+        if (cpu.eax != 0)
+            Log("BAD TRAP eax: %u", cpu.eax);
         monitor_statistic();
         return;
       }
