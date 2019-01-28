f561fc832e7600b79286ae153d364767362b4a8e
tracer-ics2018 <tracer@example.com>
2019-01-28 08:35:42 +0000

    >  compile 233333333 ics Linux 585869e0bafe 4.9.125-linuxkit x86_64 unknown unknown GNU/Linux  08:35:42 up 2 days, 24 min,  7 users,  load average: 0.04, 0.06, 0.01 bfbdf017648c512b6843e614f658989a332f963f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -207,7 +207,7 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  cpu.eflags.sf = (*result) >> 31;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
